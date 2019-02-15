4a80e93835afe179f48ec0cf289abb6c1f902964
susansu <susansu.software@gmail.com>
2019-02-15 20:58:41 +0000

    took out print statement for debugging purposes:

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,7 +22,6 @@ class FileStorage:
         """convert to json"""
         with open(self.__file_path, "w+") as f:
             new_dict = {}
-            print(self.__objects.items())
             for key, value in self.__objects.items():
                 new_dict[key] = value.to_dict()
             f.write(json.dumps(new_dict))
