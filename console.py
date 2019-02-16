55765084da5447f5a5cf3c1acaba61747bd3c9d5
stefansilverio <494@holbertonschool.com>
2019-02-16 02:07:50 +0000

    create, update, and destroy

--- console.py
+++ console.py
@@ -1,13 +1,104 @@
 #!/usr/bin/python3
 """entry point of command line interpreter"""
 import cmd, sys
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
-    def do_quit(self, arg):
-        """Quit command to exit the program"""
-        return True
+    def do_show(self, arg):
+        gary = parse(arg)
+        if (len(gary) == 0):
+            print("** class name missing **")
+        elif (len(gary) == 1 and gary[0] != "BaseModel"):
+            print("** class doesn't exist **")
+        elif (len(gary) == 1):
+            print("** instance id missing **")
+        else:
+            test_dict = storage.all()
+            key = gary[0] + "." + gary[1]
+            print(test_dict.keys())
+            if key not in test_dict.keys():
+                print("** no instance found **")
+            else:
+                print(test_dict[key])
+
+    def do_destroy(self, arg):
+        gary = parse(arg)
+        if (len(gary) == 0):
+            print("** class name missing **")
+        elif (len(gary) == 1 and gary[0] != "BaseModel"):
+            print("** class doesn't exist **")
+        elif (len(gary) == 1):
+            print("** instance id missing **")
+        else:
+            test_dict = storage.all()
+            key = gary[0] + "." + gary[1]
+            print(test_dict.keys())
+            if key not in test_dict.keys():
+                print("** no instance found **")
+            else:
+                del test_dict[key]
+                storage.save()
+
+    def do_create(self, arg):
+#        print("hi")
+  #      print(arg)
+   #     print("hi\n")
+        gary = parse(arg)
+     #   print("hi")
+    #    print(gary)
+ #       print("hi")
+
+        if (len(gary) == 0):
+            print("** class name missing **")
+        elif (len(gary) == 1 and gary[0] != "BaseModel"):
+            print("** class doesn't exist **")
+        else:
+            new_obj = BaseModel()
+            print(new_obj.id)
+            new_obj.save()  # save meth from file_storage
+
+    def do_all(self, arg):
+        gary = parse(arg)
+        if (len(gary) == 0):
+            print("** class name missing **")
+        elif (len(gary) == 1 and gary[0] != "BaseModel"):
+            print("** class doesn't exist **")
+        else:
+            print([str(v) for (k, v) in storage.all().items()])
+
+    def do_update(self, arg):
+        gary = parse(arg)
+        if (len(gary) == 0):
+            print("** class name missing **")
+
+        elif (len(gary) == 1 and gary[0] != "BaseModel"):
+            print("** class doesn't exist **")
+
+        elif (len(gary) == 3):
+            print("** no instance found **")
+
+        elif (len(gary) == 4):
+            print("** value missing **")
+
+        else:
+            test_dict = storage.all()
+            key = gary[0] + "." + gary[1]
+#            print("here's a dict!")
+#            print(test_dict.keys())
+ #           print("hi")
+            if key not in test_dict.keys():
+                print("** no instance found **")
+            else:
+                test_dict = storage.all()
+                key = gary[0] + "." + gary[1]
+                if key in test_dict:
+                    setattr(test_dict[key], gary[3], gary[4])
+                    storage.save()
+                    print(test_dict[key].__dict__)
 
     def emptyline(self):
         """do nothing on empty line"""
@@ -16,6 +107,16 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, s):
         """exit interpreter"""
         return True
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+def parse(arg):
+    'Convert a series of zero or more numbers to an argument tuple'
+#    print("arg to parse")
+ #   print(arg)
+    return tuple(map(str, arg.split()))
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
