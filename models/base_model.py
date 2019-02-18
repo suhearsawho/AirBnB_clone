6161329b5327322668aed48fdb8d09a83bac9c43
susansu <susansu.software@gmail.com>
2019-02-18 21:33:30 +0000

    will this fix the error?

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,8 @@ class BaseModel:
             for k, v in kwargs.items():
                 if k != '__class__':
                     if k in ['created_at', 'updated_at']:
-                        v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                        pass
+                        # v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, k, v)
                 if k == 'id':
                     existing = storage.all()
