aa7a35308dccb72a6dea1b4390177e64cd8c701a
susansu <susansu.software@gmail.com>
2019-02-18 22:01:59 +0000

    attempting to fix requirement errors

--- models/user.py
+++ models/user.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """create user class inherits from BaseModel"""
-
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """Defines the user class"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
