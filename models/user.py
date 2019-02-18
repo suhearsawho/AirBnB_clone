764a932ad2e69cdd4901a6e79331a2ed66ad8518
susansu <susansu.software@gmail.com>
2019-02-18 22:04:48 +0000

    added init function to see if requirement error will disappear

--- models/user.py
+++ models/user.py
@@ -6,7 +6,12 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """Defines the user class"""
 
+    """Public Class Attributes"""
     email = ''
     password = ''
     first_name = ''
     last_name = ''
+
+    def __init__(self):
+        """Initialize variables"""
+        super().__init__()
